934881777a00584607c827dfeaf2cdccf90135cc
Shaun Thornburgh <personal@shaunthornburgh.com>
2017-02-15 22:37:34 +0000

    Reservations controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,15 @@
+class ReservationsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @reservation = current_user.reservations.create(reservations_params)
+    
+    redirect_to @reservation.room
+  end
+  
+  private 
+  
+  def reservations_params
+    params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
+  end
+end
