73cf83f5dcb83dd2dcca8dbe6b046308defdc76f
Shaun Thornburgh <personal@shaunthornburgh.com>
2017-02-17 20:25:05 +0000

    Review model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def initials_of(user)
-	  user.fullname.split(' ').collect { |s| s[0] }.join(' ')
+	  user.fullname.split(' ').collect { |s| s[0] }.join('')
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   has_many :rooms
   has_many :reservations
-
+  has_many :reviews
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.fullname = auth.info.name
